c666a2da75beeef6d93bcb1558caeb2c98f501c1
Marius <marius@example.com>
2022-04-07 10:03:36 +0300

    request.hostname

--- src/index.js
+++ src/index.js
@@ -4,10 +4,21 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+const colors = ['red', 'green', 'blue'];
+
 // sukuriam endpoint
 // app.METHOD(PATH, HANDLER)
+app.get('/', (request, response) => {
+  response.send('<h1>Hello world</h1>');
+});
 app.get('/home', (request, response) => {
   response.send('Hello world');
+});
+app.get('/api/colors', (request, response) => {
+  console.log('request.hostname ===', request.hostname);
+  console.log('request.ip ===', request.ip);
+  console.log('request.method ===', request.method);
+  response.json(colors);
 });
 
 // paleidziam aplikacija, klausomes requestu
