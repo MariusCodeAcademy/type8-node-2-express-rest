e50d8bd2f4d27d4673654167553eff7d474fe4f8
Marius <marius@example.com>
2022-04-07 11:34:58 +0300

    db

---
+++ db.js
@@ -0,0 +1,22 @@
+const posts = [
+  {
+    id: 1,
+    title: 'Post 1',
+    body: 'This is Post 1 body and it is all about Post 1',
+  },
+  {
+    id: 2,
+    title: 'Post 2',
+    body: 'This is Post 2 body and it is all about Post 2',
+  },
+  {
+    id: 3,
+    title: 'Post 3',
+    body: 'This is Post 3 body and it is all about Post 3',
+  },
+  {
+    id: 4,
+    title: 'ExpressJs',
+    body: 'Express is easy way to make back end',
+  },
+];
